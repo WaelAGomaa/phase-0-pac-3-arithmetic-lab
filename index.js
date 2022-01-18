acb9d5c491e77db88f1a3dcd9be298de7fc57a89
wael <waeelash155@gmail.com>
2022-01-18 00:28:27 -0500

    done

---
+++ index.js
@@ -0,0 +1,41 @@
+number += 3;
+function add() {
+    return (number +=3);
+}
+number -= 2;
+function subtract() {
+    return (number -=2);
+}
+number *= 10;
+function multiply() {
+    return (number *=10);
+}
+number /= 5;
+function divide() {
+    return (number /= 5);
+}
+function add(a, b) {
+    return (a + b);
+}
+function subtract(a, b) {
+    return (a - b);
+}
+function multiply(a, b) {
+    return (a * b);
+}
+function divide(a, b) {
+    return (a / b);
+}
+function increment(a){
+    return (a + 1);
+}
+  
+function decrement(a){
+    return (a - 1);
+}
+function makeInt(a) {
+    return (a);
+  }
+  function parseInt(n){
+      return (n)
+  }
